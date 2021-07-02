a30d4681a5ea27091164cf9ba8948fec396d7bf1
Gabriel <gabrielhodada@gmail.com>
2021-07-02 15:58:13 +0200

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,31 +6,32 @@ import { useState } from "react";
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
+  const [word, setWord] = useState("");
 
-  const [word, setWord] = useState("")
-  
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data) =>{
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-    setWord("")
-  }
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord("");
+  };
 
   console.log(word);
-  
+
   return (
-    <div >
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+    <div>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
